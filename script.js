6acdb15278002999d2c88afe8128a99f2568b5cb
Richard <123456789+macabusrichard@gmail.com>
2024-06-07 16:16:48 +0800

    update html file with proper div child and parent update the css overall button and input design remove operator function

---
+++ script.js
@@ -0,0 +1,22 @@
+function add (num1, num2) {
+  console.log(num1, num2);
+  return alert(num1 + num2);
+}
+
+function subtract (num1, num2) {
+  return num1 - num2;
+}
+
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+
+function divide (num1, num2) {
+  return num1 / num2;
+}
+
+
+// const firstNum = num1;
+// const secondNum = num2;
+// const operator = calcu;
+
